feee097a3a8225a7cb9ecbfa0f1baa94026cb266
root <root@debian.top>
2024-05-07 09:08:16 +0700

    first commit

---
+++ app/Models/Keranjang.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Keranjang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'keranjang';
+
+    protected $fillable = [
+        'user_id',
+        'barang_id',
+        'quantity',
+    ];
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'barang_id');
+    }
+}
